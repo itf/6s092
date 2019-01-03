2e33e699d31c03aa0dfd4322ecafbfcb163c51be
Ivan Tadeu Ferreira Antunes Filho <itadeufa@gmail.com>
2019-01-02 21:11:32 -0500

    Basic files for all psets

---
+++ courses/IAP19/PS/PS11/preload.py
@@ -0,0 +1,15 @@
+cs_long_name = 'Counting Sort'
+cs_release_date = '2019-01-07:23:00'
+cs_due_date = '2019-02-06:23:00'
+#csq_allow_viewanswer = False
+csq_allow_viewexplanation = True
+
+#extra functions, to be able to do some of the problems:
+csq_funcs = {"T": (lambda c: c**3*0.6006+c**2, lambda  c:  r"%s(%s)" % ("T", ", ".join(c)) ),
+"O": (lambda c: c**3*1.6006-c**2, lambda  c:  r"%s(%s)" % ("O", ", ".join(c)) ),
+"theta": (lambda c: -c**3*0.06006+c**2*0.2, lambda  c:  r"%s(%s)" % ("\\theta", ", ".join(c)) )}
+
+
+# def csq_allow_submit(context):
+#     # only allow submissions until feb 17th at 11pm
+#     return context['cs_now'] < context['csm_time'].realize_time(context, '2018-02-17:23:00')
