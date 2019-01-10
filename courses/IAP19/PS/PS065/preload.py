bbcdad3e13bf55ac6f3368e8853bcaecfe5228de
Ivan Tadeu Ferreira Antunes Filho <itadeufa@gmail.com>
2019-01-09 21:34:59 -0500

    Releases psets!

--- courses/IAP19/PS/PS065/preload.py
+++ courses/IAP19/PS/PS065/preload.py
@@ -1,4 +1,3 @@
 cs_long_name = 'Information Theoretic Lowerbound'
-cs_release_date = '2019-01-15:23:00'
-cs_due_date = '2019-02-06:23:00'
-
+cs_release_date = '2019-01-09:21:00'
+cs_due_date = '2019-02-04:23:00'
