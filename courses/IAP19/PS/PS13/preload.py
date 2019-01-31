d0b301c6e0efcf567ec7ca70b65a75654341ceef
Justine J <justinej@mit.edu>
2019-01-31 15:12:31 -0500

    Changing all the due dates to be 2/4 not 2/6

--- courses/IAP19/PS/PS13/preload.py
+++ courses/IAP19/PS/PS13/preload.py
@@ -1,4 +1,4 @@
 cs_long_name = 'Hashing'
 cs_release_date = '2019-01-14:22:00'
-cs_due_date = '2019-02-06:23:00'
+cs_due_date = '2019-02-04:23:00'
 
--- courses/IAP19/PS/PS20/preload.py
+++ courses/IAP19/PS/PS20/preload.py
@@ -1,4 +1,4 @@
 cs_long_name = 'Breadth First Search'
 cs_release_date= '2019-01-23:23:59'
-cs_due_date = '2019-02-05:23:59'
+cs_due_date = '2019-02-04:23:59'
 
